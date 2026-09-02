0a2ead7b13ee70b8ed5402eda2dfb06b457fd709
BloodDevU <italexanderyt@gmail.com>
2026-09-02 18:45:22 +0300

    Добавление dockerfiles, dockercompose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://backend:8080',
         changeOrigin: true,
       },
     },
